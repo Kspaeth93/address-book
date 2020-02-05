ac0b8ce9663fb09054e9c9d3fbf53cc5dfa043b8
Kevin Spaeth <kevin.spaeth93@gmail.com>
2020-02-05 15:13:36 -0600

    Created settings template

---
+++ master/js/navbar.js
@@ -0,0 +1,8 @@
+Vue.component('navbar', {
+    template: '<div class="row">' +
+        '<div class="col-6 username">Username</div>' +
+        '<div class="col-6 settings">' +
+        '<input type="button" value="Settings" class="settings-button" style="float: right;" />' +
+        '</div>' +
+        '</div>'
+});
